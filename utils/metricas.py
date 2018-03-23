a2cc1369349767d1b49d59a93196c8841cf83802
diannibereval2018 <36771222+diannibereval2018@users.noreply.github.com>
2018-03-23 20:19:49 +0100

    Update metricas.py

--- utils/metricas.py
+++ utils/metricas.py
@@ -1,8 +1,17 @@
 def precision(tp,fp):
-	return tp/float(tp+fp)
+	try:
+		return tp/float(tp+fp)
+	except:
+		return 0
 
 def recall(tp,fn):
-	return tp/float(tp+fn)
+	try:
+		return tp/float(tp+fn)
+	except:
+		return 0
 
 def f1score(tp,fp,fn):
-	return (2*tp)/float(2*tp+fp+fn)
+	try:
+		return (2*tp)/float(2*tp+fp+fn)
+	except:
+		return 0
